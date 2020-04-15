4196af564c65aacc0883b450ad52ad65fa1aeec8
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-04-14 20:42:57 -0400

    first push set up skeleton

---
+++ speedtype/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>How Fast Can You Type</h1>
+      <textarea />
+      <h4>Time Left: </h4>
+      <button>Start Game </button>
+      <h2>Word Count: </h2>
+    </div>
+  );
+}
+
+export default App;
