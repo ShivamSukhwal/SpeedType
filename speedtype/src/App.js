be49d3960484454135446226a4c14cf4d6983135
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2020-04-16 15:01:25 -0400

    timer made

--- speedtype/src/App.js
+++ speedtype/src/App.js
@@ -1,23 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [timeLeft, setTimeLeft] = useState(10);
 
   const handleClick = (e) => {
     const { value } = e.target;
     setText(value);
   };
 
-  console.log(text);
+  const calculateWordCount = (text) => {
+    const wordCount = text.trim().split(" ");
+    return wordCount.filter((word) => word !== "").length;
+  };
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      setTimeout(() => {
+        setTimeLeft((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeLeft]);
 
   return (
     <div>
       <h1>How Fast Can You Type</h1>
       <textarea onChange={handleClick} value={text} />
-      <h4>Time Left: </h4>
+      <h4>Time Left: {timeLeft} </h4>
       <button>Start Game </button>
-      <h2>Word Count: </h2>
+      <h2>Word Count: {() => calculateWordCount(text)} </h2>
     </div>
   );
 }
